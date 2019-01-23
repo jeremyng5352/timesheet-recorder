2c4889af8b80c0685c9bf56e845d585951b3d97f
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-01-23 16:19:54 +1000

    Function to retrieve value in sign up form and validate

--- src/app/home/components/sign-up/sign-up.component.ts
+++ src/app/home/components/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,17 +7,54 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-  signupForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-  });
-
-  constructor() { }
+  signupForm: FormGroup;
+  isPasswordMatch;
+  timeout = null;
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  buildForm() {
+    this.signupForm = this.formBuilder.group({
+      username: this.formBuilder.control('', [Validators.required, Validators.minLength(6)]),
+      password: this.formBuilder.control('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: this.formBuilder.control('', [Validators.required]),
+      // confirmPassword: this.formBuilder.control('', [Validators.required, Validators.minLength(6)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.minLength(6), Validators.email]),
+      phone: this.formBuilder.control('', [Validators.required, Validators.minLength(10)])
+    });
+  }
+
+  startPasswordMatching() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      const detail = this.getFormValue();
+      this.comparePassword(detail);
+    }, 300);
+  }
+
+  getFormValue() {
+    const username = this.signupForm.get('username').value;
+    const password = this.signupForm.get('password').value;
+    const confirmPassword = this.signupForm.get('confirmPassword').value;
+    const email = this.signupForm.get('email').value;
+    const phone = this.signupForm.get('phone').value;
+    return { username, password, confirmPassword, email, phone };
+  }
+
+  comparePassword(detail) {
+    if (detail.password !== detail.confirmPassword) {
+      this.isPasswordMatch = false;
+    } else {
+      this.isPasswordMatch = true;
+    }
+  }
+
+  signup() {
+    const detail = this.getFormValue();
+  }
 }
---
+++ src/app/user/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    username: string;
+    password: string;
+    email: string;
+    phone: number;
+
+    constructor() {
+
+    }
+}
